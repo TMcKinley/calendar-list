3204da7ead3d9e5ed6cd760834905c677e1b40a2
tmckinley <tmckinley@qualitylogoproducts.com>
2020-05-04 22:45:36 -0700

    Remove unused code

--- src/App.vue
+++ src/App.vue
@@ -3,49 +3,25 @@
     <v-content>
       <v-container>
         <v-layout row justify-center class="ma-5">
-          <v-card style="width:700px;">
+          <v-card style="width:1400px;">
             <v-toolbar color="blue darken-4" dark>
               <v-toolbar-title class="headline">
                 Calendar Checklist
               </v-toolbar-title>
             </v-toolbar>
 
-            <v-container>
-              <div class="d-flex justify-space-between align-center">
-                <v-subheader class="pl-1 headline">{{
-                  calculateDate()
-                }}</v-subheader>
-
-                <div>{{ filteredTodos.length }} Tasks</div>
-              </div>
-            </v-container>
-
-            <v-divider class="ml-4 mr-4" style="height:1px;"></v-divider>
-
-            <v-list two-line subheader>
-              <v-subheader inset class="subheading" v-if="todos.length == 0"
-                >You have 0 Tasks due today, congrats!</v-subheader
-              >
-
-              <v-list-item dense v-for="(todo, i) in filteredTodos" :key="i">
-                <v-list-item-icon class="mt-0 mr-2">
-                  <v-checkbox
-                    class="red--text"
-                    off-icon="fas fa-square"
-                    @click="onCheckClick(todo, $event)"
-                  ></v-checkbox>
-                </v-list-item-icon>
-
-                <v-list-item-content>
-                  <v-list-item-title class="title"
-                    >{{ todo.description }}
-                  </v-list-item-title>
-                  <v-list-item-subtitle
-                    >Start: {{ formatDate(todo.start_time) }}
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
+            <div class="d-flex">
+              <TodoList
+                :style="leftStyleObject"
+                :date="currentDate"
+                :todos="todos"
+              ></TodoList>
+              <TodoList
+                :style="rightStyleObject"
+                :date="nextDate"
+                :todos="todos"
+              ></TodoList>
+            </div>
           </v-card>
         </v-layout>
       </v-container>
@@ -54,7 +30,13 @@
 </template>
 
 <script>
+import TodoList from "./components/TodoList";
+
 export default {
+  components: {
+    TodoList
+  },
+
   data() {
     return {
       todos: []
@@ -62,69 +44,38 @@ export default {
   },
 
   computed: {
-    filteredTodos: function() {
-      // Jank to get a date in this format: 2020-05-02.
-      let rawDate = new Intl.DateTimeFormat("en-GB")
-        .format(new Date())
-        .split("/")
-        .reverse()
-        .join("-");
+    leftStyleObject: function() {
+      let obj = {};
+      obj.flex = "70";
+      if (this.isWeekend(this.currentDate)) {
+        obj.flex = "50";
+      }
 
-      let notCompleted = this.todos.filter(obj => obj.completed === 0);
-      let filteredArray = notCompleted.filter(obj => {
-        let datePart = obj.start_time.split("T")[0];
+      return obj;
+    },
+    rightStyleObject: function() {
+      let obj = {};
+      obj.flex = "30";
+      if (this.isWeekend(this.nextDate)) {
+        obj.flex = "50";
+      }
 
-        return rawDate === datePart;
-      });
+      return obj;
+    },
+    currentDate: function() {
+      return new Date();
+    },
+    nextDate: function() {
+      const today = new Date();
+      const tomorrow = new Date(today);
+      tomorrow.setDate(tomorrow.getDate() + 1);
 
-      return filteredArray;
+      return tomorrow;
     }
   },
   methods: {
-    calculateDate: function() {
-      const date = new Date();
-      const formatter = new Intl.DateTimeFormat("en-US", {
-        weekday: "long",
-        month: "long",
-        day: "numeric"
-      });
-
-      const fmtDate = formatter.format(date);
-
-      return fmtDate + this.nth(date.getDate()) + " (Today)";
-    },
-    formatDate: function(rawDate) {
-      const date = new Date(rawDate);
-      const formatter = new Intl.DateTimeFormat("en-US", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit"
-      });
-
-      return formatter.format(date);
-    },
-    nth(d) {
-      if (d > 3 && d < 21) return "th";
-      switch (d % 10) {
-        case 1:
-          return "st";
-        case 2:
-          return "nd";
-        case 3:
-          return "rd";
-        default:
-          return "th";
-      }
-    },
-    onCheckClick: function(dataObject, $event) {
-      if (dataObject.completed === 0) {
-        dataObject.completed = 1;
-      }
-      $event.preventDefault();
-      $event.stopPropagation();
+    isWeekend: function(rawDate) {
+      return rawDate.getDay() === 6 || rawDate.getDay() === 0;
     }
   },
   created() {
---
+++ src/components/TodoList.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+    <v-container>
+      <div class="d-flex justify-space-between align-center">
+        <v-subheader class="pl-1 headline">{{ calculateDate() }}</v-subheader>
+
+        <div>{{ filteredTodos.length }} Tasks</div>
+      </div>
+    </v-container>
+
+    <v-divider class="ml-4 mr-4" style="height:1px;"></v-divider>
+
+    <v-list two-line subheader>
+      <v-subheader inset class="subheading" v-if="todos.length == 0"
+        >You have 0 Tasks due today, congrats!</v-subheader
+      >
+
+      <v-list-item dense v-for="(todo, i) in filteredTodos" :key="i">
+        <v-list-item-icon class="mt-0 mr-2">
+          <v-checkbox
+            class="primary--text"
+            off-icon="fas fa-square"
+            @click="onCheckClick(todo, $event)"
+          ></v-checkbox>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title class="title"
+            >{{ todo.description }}
+          </v-list-item-title>
+          <v-list-item-subtitle
+            >Start: {{ formatDate(todo.start_time) }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    date: {
+      type: Date,
+      required: true
+    },
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+
+  data() {
+    return {};
+  },
+
+  computed: {
+    isToday: function() {
+      return this.date.getDate() === new Date().getDate()
+        ? "Today"
+        : "Tomorrow";
+    },
+    filteredTodos: function() {
+      // Jank to get a date in this format: 2020-05-02.
+      let rawDate = new Intl.DateTimeFormat("en-GB")
+        .format(this.date)
+        .split("/")
+        .reverse()
+        .join("-");
+
+      let notCompleted = this.todos.filter(obj => obj.completed === 0);
+      let filteredArray = notCompleted.filter(obj => {
+        let datePart = obj.start_time.split("T")[0];
+
+        return rawDate === datePart;
+      });
+
+      return filteredArray;
+    }
+  },
+  methods: {
+    calculateDate: function() {
+      const date = this.date;
+      const formatter = new Intl.DateTimeFormat("en-US", {
+        weekday: "long",
+        month: "long",
+        day: "numeric"
+      });
+
+      const fmtDate = formatter.format(date);
+
+      return fmtDate + this.nth(date.getDate()) + ` (${this.isToday})`;
+    },
+    formatDate: function(rawDate) {
+      const date = new Date(rawDate);
+      const formatter = new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+      });
+
+      return formatter.format(date);
+    },
+    nth(d) {
+      if (d > 3 && d < 21) return "th";
+      switch (d % 10) {
+        case 1:
+          return "st";
+        case 2:
+          return "nd";
+        case 3:
+          return "rd";
+        default:
+          return "th";
+      }
+    },
+    onCheckClick: function(dataObject, $event) {
+      if (dataObject.completed === 0) {
+        dataObject.completed = 1;
+      }
+      $event.preventDefault();
+      $event.stopPropagation();
+    }
+  },
+  mounted: function() {}
+};
+</script>
+
+<style></style>
